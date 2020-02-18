550658cdec623637995ffaad479069f1b16ddb94
Jussi Maristo <jussi.maristo@perfektio.fi>
2020-02-18 13:39:18 +0200

    added ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import MapContainer from './components/mapContainer'
+
+const App = () => {
+  return (
+    <div className="App">
+      <MapContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ ui/src/components/mapContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MapContainer = () => {
+  return <div>MAPPI</div>
+}
+
+export default MapContainer
