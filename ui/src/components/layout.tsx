d3c9fb635e0023731dd42c4fe1f1977c30d0c1c1
Jussi Maristo <jussi.maristo@perfektio.fi>
2020-02-21 22:37:18 +0200

    Added map stuff

--- ui/src/components/layout.tsx
+++ ui/src/components/layout.tsx
@@ -4,37 +4,29 @@ import {
   VerticalSection,
   VerticalItem,
 } from 'react-rainbow-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnchor } from '@fortawesome/free-solid-svg-icons'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div style={{ height: '100vh' }}>
-      <div
-        style={{
-          height: '50px',
-          backgroundImage: 'linear-gradient(#c7bfbf, white)',
-          display: 'flex',
-          alignItems: 'center',
-          padding: '0 50px',
-        }}
-      >
-        SadamPåCarto
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ width: '220px', height: '100%' }}>
+        <VerticalNavigation compact>
+          <VerticalSection>
+            <VerticalItem
+              name="item-1"
+              label="SadamPåCarto"
+              icon={<FontAwesomeIcon icon={faAnchor} />}
+            />
+            <VerticalItem name="item-1" label="Juttu" />
+            <VerticalItem name="item-2" label="Toka Juttu" />
+            <VerticalItem name="item-3" label="Tässä on Tää" />
+            <VerticalItem name="item-4" label="Tuolla on Toi" />
+          </VerticalSection>
+        </VerticalNavigation>
       </div>
 
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: '220px', height: '100%' }}>
-          <VerticalNavigation compact>
-            <VerticalSection>
-              <VerticalItem name="item-1" label="Recent" />
-              <VerticalItem name="item-2" label="Projects" />
-              <VerticalItem name="item-3" label="Folders" />
-              <VerticalItem name="item-4" label="Settings" />
-              <VerticalItem name="item-5" label="Reports" />
-            </VerticalSection>
-          </VerticalNavigation>
-        </div>
-
-        <div style={{ flex: 1, height: '100%' }}>{children}</div>
-      </div>
+      <div style={{ flex: 1, height: '100%' }}>{children}</div>
     </div>
   )
 }
--- ui/src/components/mapContainer.tsx
+++ ui/src/components/mapContainer.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
+type LatLong = {
+  lat: number
+  lng: number
+}
+
 const MapContainer = () => {
-  const position: [number, number] = [60.19, 24.94]
+  const [markers, setMarkers] = useState<LatLong[]>([])
+  const position: LatLong = { lat: 60.19, lng: 24.94 }
+
+  const addMarker = (e: { latlng: LatLong }) => {
+    setMarkers([...markers, e.latlng])
+  }
+
+  console.log({ markers })
+  const markersToMap = markers.map((latlong: LatLong) => (
+    <Marker position={latlong} key={'' + latlong.lat + latlong.lng}>
+      <Popup>
+        <span>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </span>
+      </Popup>
+    </Marker>
+  ))
 
   return (
-    <div style={{ height: '500px' }}>
-      <Map center={position} zoom={13}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <TileLayer url="https://t1.openseamap.org/seamark/{z}/{x}/{y}.png" />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup.
-            <br />
-            Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    </div>
+    <Map onClick={addMarker} center={position} zoom={13}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer url="https://t1.openseamap.org/seamark/{z}/{x}/{y}.png" />
+      {markersToMap}
+    </Map>
   )
 }
 
