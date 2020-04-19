b78a173f80dd4bdeb0cd4d4f6782d69096f34543
Jussi Maristo <jussi.maristo@perfektio.fi>
2020-04-19 07:42:16 +0300

    linter config etc

--- ui/src/components/layout.tsx
+++ ui/src/components/layout.tsx
@@ -1,34 +1,32 @@
+import { faAnchor } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { ReactNode } from 'react'
 import {
+  VerticalItem,
   VerticalNavigation,
   VerticalSection,
-  VerticalItem,
 } from 'react-rainbow-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAnchor } from '@fortawesome/free-solid-svg-icons'
 
-const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <div style={{ width: '220px', height: '100%' }}>
-        <VerticalNavigation compact>
-          <VerticalSection>
-            <VerticalItem
-              name="item-1"
-              label="SadamPåCarto"
-              icon={<FontAwesomeIcon icon={faAnchor} />}
-            />
-            <VerticalItem name="item-1" label="Juttu" />
-            <VerticalItem name="item-2" label="Toka Juttu" />
-            <VerticalItem name="item-3" label="Tässä on Tää" />
-            <VerticalItem name="item-4" label="Tuolla on Toi" />
-          </VerticalSection>
-        </VerticalNavigation>
-      </div>
-
-      <div style={{ flex: 1, height: '100%' }}>{children}</div>
+const Layout = ({ children }: { children: ReactNode }) => (
+  <div style={{ display: 'flex', height: '100vh' }}>
+    <div style={{ width: '220px', height: '100%' }}>
+      <VerticalNavigation compact>
+        <VerticalSection>
+          <VerticalItem
+            name="item-1"
+            label="SadamPåCarto"
+            icon={<FontAwesomeIcon icon={faAnchor} />}
+          />
+          <VerticalItem name="item-1" label="Juttu" />
+          <VerticalItem name="item-2" label="Toka Juttu" />
+          <VerticalItem name="item-3" label="Tässä on Tää" />
+          <VerticalItem name="item-4" label="Tuolla on Toi" />
+        </VerticalSection>
+      </VerticalNavigation>
     </div>
-  )
-}
+
+    <div style={{ flex: 1, height: '100%' }}>{children}</div>
+  </div>
+)
 
 export default Layout
