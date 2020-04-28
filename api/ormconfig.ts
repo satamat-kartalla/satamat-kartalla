02434a2f2f444aa1a5223d89e643706b776a9c97
Jussi Maristo <jussi.maristo@perfektio.fi>
2020-04-28 20:37:10 +0300

    ormconfig fix

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -1,9 +1,17 @@
+/**
+ * change localUri if needed
+ */
+const localUri = 'postgres://:@localhost:5432/satamat-kartalla';
+
+let DB_URI = null;
+if (process.env.DATABASE_URL) {
+  DB_URI = process.env.DATABASE_URL;
+} else {
+  DB_URI = localUri;
+}
+
+export const url = DB_URI;
 export const type = 'postgres';
-export const host = process.env.TYPEORM_HOST;
-export const port = 5432;
-export const username = '';
-export const password = '';
-export const database = 'satamat-kartalla';
 export const entities = ['src/entity/*.ts'];
 export const logging = true;
 export const synchronize = true;
